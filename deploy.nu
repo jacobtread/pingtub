6b5010181e06bd1a16853823595a40abd4305642
Jacobtread <jacobtread@gmail.com>
2024-03-04 12:00:16 +1300

    feat: initial source rendering

---
+++ deploy.nu
@@ -0,0 +1,2 @@
+cargo build
+cp "./target/debug/pingtub.dll" "C:/Program Files/obs-studio/obs-plugins/64bit/pingtub.dll"
--- src/lib.rs
+++ src/lib.rs
@@ -1,64 +1,40 @@
 use obs_wrapper::{
     // Macro for registering modules
+    graphics::{GraphicsColorFormat, GraphicsTexture},
     obs_register_module,
-    // Macro for creating strings
     obs_string,
-    // Everything required for modules
+    obs_sys,
     prelude::*,
-    // Everything required for creating a source
     source::*,
 };
 
-// The module that will handle creating the source.
-struct TestModule {
-    context: ModuleContext,
+/// Module that provides the [TuberSource]
+struct TuberModule {
+    /// Underlying module context
+    ctx: ModuleContext,
 }
 
-// The source that will be shown inside OBS.
-struct TestSource;
-
-// Implement the Sourceable trait for TestSource, this is required for each source.
-// It allows you to specify the source ID and type.
-impl Sourceable for TestSource {
-    fn get_id() -> ObsString {
-        obs_string!("test_source")
-    }
-
-    fn get_type() -> SourceType {
-        SourceType::INPUT
-    }
-
-    fn create(create: &mut CreatableSourceContext<Self>, source: SourceContext) -> Self {
-        Self
-    }
-}
-
-// Allow OBS to show a name for the source
-impl GetNameSource for TestSource {
-    fn get_name() -> ObsString {
-        obs_string!("Test Source")
-    }
-}
-
-// Implement the Module trait for TestModule. This will handle the creation of the source and
-// has some methods for telling OBS a bit about itself.
-impl Module for TestModule {
-    fn new(context: ModuleContext) -> Self {
-        Self { context }
+impl Module for TuberModule {
+    fn new(ctx: ModuleContext) -> Self {
+        Self { ctx }
     }
 
     fn get_ctx(&self) -> &ModuleContext {
-        &self.context
+        &self.ctx
     }
 
-    // Load the module - create all sources, returning true if all went well.
     fn load(&mut self, load_context: &mut LoadContext) -> bool {
         // Create the source
         let source = load_context
-            .create_source_builder::<TestSource>()
+            .create_source_builder::<TuberSource>()
             // Since GetNameSource is implemented, this method needs to be called to
             // enable it.
             .enable_get_name()
+            .enable_update()
+            .enable_video_render()
+            .enable_video_tick()
+            .enable_get_width()
+            .enable_get_height()
             .build();
 
         // Tell OBS about the source so that it will show it.
@@ -69,16 +45,75 @@ impl Module for TestModule {
     }
 
     fn description() -> ObsString {
-        obs_string!("A great test module.")
+        obs_string!("Png-tuber OBS integration")
     }
 
     fn name() -> ObsString {
-        obs_string!("Test Module")
+        obs_string!("Ping-tub")
     }
 
     fn author() -> ObsString {
-        obs_string!("Bennett")
+        obs_string!("Jacobtread")
     }
 }
 
-obs_register_module!(TestModule);
+/// Input source that render the png-tuber model
+struct TuberSource {
+    source: SourceContext,
+    texture: GraphicsTexture,
+}
+static DATA: &[u8] = &[255u8; 512 * 512 * 4];
+
+impl Sourceable for TuberSource {
+    // Unique source ID
+    fn get_id() -> ObsString {
+        obs_string!("tuber_source")
+    }
+
+    // Source is a video input source
+    fn get_type() -> SourceType {
+        SourceType::INPUT
+    }
+
+    fn create(create: &mut CreatableSourceContext<Self>, source: SourceContext) -> Self {
+        let mut texture = GraphicsTexture::new(512, 512, GraphicsColorFormat::RGBA);
+
+        texture.set_image(DATA, 512 * 4, false);
+
+        Self { source, texture }
+    }
+}
+
+impl GetWidthSource for TuberSource {
+    fn get_width(&mut self) -> u32 {
+        512
+    }
+}
+
+impl GetHeightSource for TuberSource {
+    fn get_height(&mut self) -> u32 {
+        512
+    }
+}
+
+impl VideoRenderSource for TuberSource {
+    fn video_render(&mut self, context: &mut GlobalContext, render: &mut VideoRenderContext) {
+        self.texture.draw(0, 0, 512, 512, false);
+    }
+}
+
+impl VideoTickSource for TuberSource {
+    fn video_tick(&mut self, seconds: f32) {}
+}
+
+impl UpdateSource for TuberSource {
+    fn update(&mut self, settings: &mut DataObj, context: &mut GlobalContext) {}
+}
+
+impl GetNameSource for TuberSource {
+    fn get_name() -> ObsString {
+        obs_string!("Ping-Tuber Source")
+    }
+}
+
+obs_register_module!(TuberModule);
