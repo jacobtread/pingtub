abc3ae3fa01bf27bec0018f1c3f30c2e9de65572
Jacobtread <jacobtread@gmail.com>
2024-03-04 13:33:28 +1300

    feat: mvp dark and light image

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+use std::{
+    io::{BufReader, Cursor},
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+};
+
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    Sample, Stream,
+};
 use obs_wrapper::{
     // Macro for registering modules
     graphics::{GraphicsColorFormat, GraphicsTexture},
@@ -61,8 +73,22 @@ impl Module for TuberModule {
 struct TuberSource {
     source: SourceContext,
     texture: GraphicsTexture,
+
+    buffer: RgbaImage,
+    buffer_speaking: RgbaImage,
+
+    buffer_index: i8,
+    ticks: usize,
+
+    speaking: Arc<AtomicBool>,
+
+    stream: Stream,
 }
+
 static DATA: &[u8] = &[255u8; 512 * 512 * 4];
+static TEST_IMAGE_DATA: &[u8] = include_bytes!("../test/jacobtread.png");
+use image::{io::Reader as ImageReader, EncodableLayout, ImageBuffer, Rgba, RgbaImage};
+const THRESHOLD: f32 = 0.1; // Adjust the threshold as needed
 
 impl Sourceable for TuberSource {
     // Unique source ID
@@ -80,7 +106,59 @@ impl Sourceable for TuberSource {
 
         texture.set_image(DATA, 512 * 4, false);
 
-        Self { source, texture }
+        let img = ImageReader::new(Cursor::new(TEST_IMAGE_DATA))
+            .with_guessed_format()
+            .unwrap()
+            .decode()
+            .unwrap();
+        let img = img.resize(512, 512, image::imageops::FilterType::Nearest);
+
+        let buffer_speaking = img.to_rgba8();
+        let img = img.clone().brighten(-50);
+        let buffer = img.to_rgba8();
+
+        let mut texture =
+            GraphicsTexture::new(img.width(), img.height(), GraphicsColorFormat::RGBA);
+
+        let speaking = Arc::new(AtomicBool::new(false));
+
+        let host = cpal::default_host();
+        let input_device = host.default_input_device().unwrap();
+        let input_config = input_device.default_input_config().unwrap().into();
+
+        let speaking_used = speaking.clone();
+
+        let stream = input_device
+            .build_input_stream(
+                &input_config,
+                move |data: &[f32], _| {
+                    // Calculate amplitude (root mean square)
+                    let amplitude = data.iter().fold(0.0, |sum, &sample| sum + sample * sample);
+                    let amplitude = (amplitude / data.len() as f32).sqrt();
+
+                    // Check if amplitude exceeds threshold
+                    let is_speaking = amplitude > THRESHOLD;
+
+                    // Update speaking status
+                    speaking_used.store(is_speaking, Ordering::SeqCst);
+                },
+                |err| {},
+                None,
+            )
+            .unwrap();
+
+        stream.play().unwrap();
+
+        Self {
+            source,
+            texture,
+            speaking,
+            buffer,
+            buffer_index: -1,
+            ticks: 0,
+            buffer_speaking,
+            stream,
+        }
     }
 }
 
@@ -98,12 +176,27 @@ impl GetHeightSource for TuberSource {
 
 impl VideoRenderSource for TuberSource {
     fn video_render(&mut self, context: &mut GlobalContext, render: &mut VideoRenderContext) {
-        self.texture.draw(0, 0, 512, 512, false);
+        self.texture.draw(0, 0, 0, 0, false);
     }
 }
 
 impl VideoTickSource for TuberSource {
-    fn video_tick(&mut self, seconds: f32) {}
+    fn video_tick(&mut self, seconds: f32) {
+        let speaking = self.speaking.load(std::sync::atomic::Ordering::SeqCst);
+
+        if self.buffer_index != 1 && speaking {
+            self.texture.set_image(
+                self.buffer_speaking.as_bytes(),
+                self.buffer_speaking.width() * 4,
+                false,
+            );
+            self.buffer_index = 1;
+        } else if self.buffer_index != 0 && !speaking {
+            self.texture
+                .set_image(self.buffer.as_bytes(), self.buffer.width() * 4, false);
+            self.buffer_index = 0;
+        }
+    }
 }
 
 impl UpdateSource for TuberSource {
